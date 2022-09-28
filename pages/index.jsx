88facb2b209c32259de6b059cf52b3adf488f4ff
ZNTKSGrA1P <hajhkn4@gmail.com>
2022-09-28 11:09:34 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "V6S_xh0vVkMDfdTSpj78gkzT_PejnmJA"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "V6S_xh0vVkMDfdTSpj78gkzT_PejnmJA"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
